9a0418c336d9d9e48b224414367f04d3668d3d7e
klaudi-kolnikaj <74505365+klaudi-kolnikaj@users.noreply.github.com>
2021-04-11 23:45:36 +0200

    Create gamelib.h

--- gamelib.h
+++ gamelib.h
@@ -1,5 +1,28 @@
-// Dichiarazione delle funzioni da chiamare in main.c
-// Definizione dei tipi necessari alla libreria
+#include <stdbool.h>
+
+enum Tipo_stanza {vuota, quest_semplice, quest_complicata, botola};
+
+struct Stanza {
+    struct Stanza * avanti;
+    struct Stanza * sinistra;
+    struct Stanza * destra;
+    struct Stanza * precedente;
+    enum Tipo_stanza tipo;
+    bool emergenza;
+  };
+
+  enum Stato_giocatore {astronauta, impostore, assassinato, defenestrato};
+  enum Nome_giocatore {rosso, arancione, giallo, rosa, nero, celeste, marrone,blu, bianco, verde};
+
+  struct Giocatore {
+     enum Nome_giocatore nome;
+     enum Stato_giocatore stato;
+     struct Stanza * posizione;
+  };
+
+    void imposta_gioco();
+    void gioca();
+    void termina_gioco();
 
 
 
