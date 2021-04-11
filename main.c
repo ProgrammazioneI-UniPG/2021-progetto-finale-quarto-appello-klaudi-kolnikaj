7c28057b92e6ad2c6da8969abc1c79e23ecfb9ee
klaudi-kolnikaj <74505365+klaudi-kolnikaj@users.noreply.github.com>
2021-04-11 23:43:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "gamelib.h"
 
-// Questo file continene solo la definizione della funzione main
-// con il menu principale 1-2-3
+//MENU
+void menu();
+int main(void){
+int scelta;
 
-int main( ) {
+do {
+  printf("SELEZIONARE UN'OPZIONE");
+  printf("*********MENU******");
+  printf("*1) IMPOSTA GIOCO *");
+  printf("*2) GIOCA         *");
+  printf("*3) TERMIINA GIOCO*");
+  printf("*******************");
+  scanf("%d", &scelta);
+  while(getchar()=='\n');
 
 
-
+  switch(scelta){
+    case 1:
+        printf("HAI SELEZIONATO IMPOSTA GIOCO\n");
+        break;
+    case 2:
+        printf("HAI SELEZIONATO GIOCA\n");
+        break;
+    case 3:
+        printf("HAI SELEZIONATO TERMINA GIOCO\n");
+        break;
+    default:
+        printf("Il comando è sbagliato\n");
+        break;
+}
+}
+while(scelta < 4);
 }
 
 
